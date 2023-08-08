cef6e4834cf9ebb2d9554f6bd0d6425c18dc0c84
EmitoStuer <stuer.87@gmail.com>
2023-08-08 11:58:23 -0300

    Probando modificar.
    Cambios en linia 16 y 17

--- Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
+++ Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
@@ -9,10 +9,12 @@ public class Ejercicio1Encuentros1al3App {
         System.out.println("Primera prueba iniciada. Yanina");
         System.out.println("Golazo de Messi. Andy");
         System.out.println("Aguante la ULP");
-        System.out.println("holaaaqa");
+        System.out.println("holaaaa");
         // Segunda modificación de prueba
         
         System.out.println("Prueba 2");
+        //Hola Familia.
+        System.out.println("Probando a ver que sale.");
         
     }
     

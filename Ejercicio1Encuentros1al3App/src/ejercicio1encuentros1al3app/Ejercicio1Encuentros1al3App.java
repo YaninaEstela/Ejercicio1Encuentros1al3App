45a36f57198d1846b56fd01458bef23c98b1ad79
Andres <andyolguint720@gmail.com>
2023-08-08 11:44:50 -0300

    Actualización
    Argentina

--- Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
+++ Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
@@ -8,6 +8,7 @@ public class Ejercicio1Encuentros1al3App {
          
         System.out.println("Primera prueba iniciada. Yanina");
         System.out.println("Golazo de Messi. Andy");
+        System.out.println("Aguante la ULP");
     }
     
 }

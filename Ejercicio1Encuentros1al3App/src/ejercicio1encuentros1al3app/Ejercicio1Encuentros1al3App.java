ac15a6aa1f4a4b575b4d8df2e7f7a2fb00994996
Mist3rN <niba.lop@gmail.com>
2023-08-08 12:06:59 -0300

    ayuda
    blablabla

--- Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
+++ Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
@@ -8,7 +8,7 @@ public class Ejercicio1Encuentros1al3App {
          
         System.out.println("Primera prueba iniciada. Yanina");
         System.out.println("Golazo de Messi. Any");
-        System.out.println(" como era parabajar por primera vez al netbeans?");
+       
         System.out.println("Golazo de Messi. Andy");
         System.out.println("Aguante la ULP");
         System.out.println("holaaaa");
@@ -18,7 +18,8 @@ public class Ejercicio1Encuentros1al3App {
         //Hola Familia.
         System.out.println("Probando a ver que sale. soy Emi");
         
-
+ System.out.println(" como era parabajar por primera vez al netbeans?");
     }
+    
     
 }

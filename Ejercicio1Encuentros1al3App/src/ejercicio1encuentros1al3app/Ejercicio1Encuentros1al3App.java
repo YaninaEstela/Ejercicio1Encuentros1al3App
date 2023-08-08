6d116cad07d163e7defb20540f4ab000bef016a7
YaninaEstela <yaninaestelataylor@gmail.com>
2023-08-08 11:20:17 -0300

    Primera prueba de App
    Este es el ejercicio 1 de los Encuentros 1 al 3 de la Guía 1 de Aplicaciones

---
+++ Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
@@ -0,0 +1,13 @@
+
+package ejercicio1encuentros1al3app;
+
+public class Ejercicio1Encuentros1al3App {
+
+    
+    public static void main(String[] args) {
+         
+        System.out.println("Primera prueba iniciada. Yanina");
+        
+    }
+    
+}

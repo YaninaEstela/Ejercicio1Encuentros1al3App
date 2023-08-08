bf721813f8d5fdba21887debc5aefa9b9a36cec4
geroiba3 <geronimo.ibanez@davinci.edu.ar>
2023-08-08 11:52:07 -0300

    actualizacion

--- Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
+++ Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
@@ -9,7 +9,7 @@ public class Ejercicio1Encuentros1al3App {
         System.out.println("Primera prueba iniciada. Yanina");
         System.out.println("Golazo de Messi. Andy");
         System.out.println("Aguante la ULP");
-        
+        System.out.println("holaaaqa");
         // Segunda modificación de prueba
         
         System.out.println("Prueba 2");

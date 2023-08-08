da1ae28d8562180458eb78eb54edfa8fb4e671ec
Andres <andyolguint720@gmail.com>
2023-08-08 11:29:24 -0300

    De Andy
    Agregado un SOUT

--- Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
+++ Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
@@ -7,7 +7,7 @@ public class Ejercicio1Encuentros1al3App {
     public static void main(String[] args) {
          
         System.out.println("Primera prueba iniciada. Yanina");
-        
+        System.out.println("Golazo de Messi. Any");
     }
     
 }

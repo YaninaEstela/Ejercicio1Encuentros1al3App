da1f32787f12fc7805fd23cc163761ab233a3fe1
Verocgreco <veronicacgreco@outlook.com>
2023-08-08 12:23:12 -0300

    Probando otra vez

--- Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
+++ Ejercicio1Encuentros1al3App/src/ejercicio1encuentros1al3app/Ejercicio1Encuentros1al3App.java
@@ -1,26 +1,24 @@
-
 package ejercicio1encuentros1al3app;
 
 public class Ejercicio1Encuentros1al3App {
 
-    
     public static void main(String[] args) {
-         
+
         System.out.println("Primera prueba iniciada. Yanina");
         System.out.println("Golazo de Messi. Any");
-       
+
         System.out.println("Golazo de Messi. Andy");
         System.out.println("Aguante la ULP");
         System.out.println("holaaaa soy gero xd");
         // Segunda modificación de prueba
-        
+
         System.out.println("Prueba 2");
         //Hola Familia.
         System.out.println("Probando a ver que sale. soy Emi");
         System.out.println("Hola chicos, soy Vero");
-        
- System.out.println(" como era parabajar por primera vez al netbeans?//Ariel");
+
+        System.out.println(" como era parabajar por primera vez al netbeans?//Ariel");
+        System.out.println("Probando otra vez");
     }
-    
-    
+
 }
